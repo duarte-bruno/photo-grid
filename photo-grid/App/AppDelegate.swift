7718130baf9538e5329db8abe3b3f951a0b19c28
Bruno Duarte <brunopereiras@icloud.com>
2021-02-25 12:37:49 -0300

    Created the animated splash screen.

--- photo-grid/App/AppDelegate.swift
+++ photo-grid/App/AppDelegate.swift
@@ -15,7 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = TabBarController()
+        
+        let navigation = UINavigationController(rootViewController: InitialViewController())
+        navigation.setNavigationBarHidden(true, animated: false)
+        
+        window?.rootViewController = navigation
         window?.makeKeyAndVisible()
 
         return true
---
+++ photo-grid/Controllers/InitialViewController.swift
@@ -0,0 +1,59 @@
+//
+//  InitialViewController.swift
+//  photo-grid
+//
+//  Created by Bruno Duarte on 25/02/21.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    // MARK: - Attributes
+    
+    @IBOutlet private weak var logoImage: UIImageView!
+    @IBOutlet private weak var madeByLabel: UILabel!
+    @IBOutlet private weak var nameLabel: UILabel!
+    
+    // MARK: - Class lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showMainScreen()
+        startAnimation()
+    }
+    
+    // MARK: - Logic
+    
+    private func setup() {
+        madeByLabel.textColor = Constants.color().lightGray
+        nameLabel.textColor = Constants.color().blue
+        view.backgroundColor = Constants.color().black
+    }
+    
+    private func startAnimation() {
+        UIView.animate(withDuration: 0.7, animations: {
+            self.logoImage.alpha = 1.0
+        }, completion: { _ in
+            UIView.animate(withDuration: 0.7) {
+                self.madeByLabel.alpha = 1.0
+                self.nameLabel.alpha = 1.0
+            }
+        })
+    }
+    
+    private func showMainScreen() {
+        let seconds = 2.0
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+            let tabBarController = TabBarController()
+            tabBarController.modalPresentationStyle = .fullScreen
+            tabBarController.modalTransitionStyle = .crossDissolve
+            self.navigationController?.present(tabBarController, animated: true, completion: nil)
+        }
+    }
+}
